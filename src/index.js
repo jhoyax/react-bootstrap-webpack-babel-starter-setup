9526f23c874207fbc0452c33e28c7f8d462ff887
Joy Christine <joylopenaasis@gmail.com>
2019-07-19 11:27:45 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>React, Bootstrap, Webpack 4 & Babel 7!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
